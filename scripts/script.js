a85e63c4237dc5e09b925696c01e95d1940eb5e0
Christian Sales <christiansales07@outlook.com>
2023-11-08 23:05:55 -0300

    m

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,8 @@ const popupName=popup.querySelector(".popup__input_name");
 const popupDescription=popup.querySelector(".popup__input_description");
 const buttonSave=popup.querySelector(".popup__button");
 const buttonAdd=profile.querySelector(".profile__button-add");
-const popupAdd=document.querySelector(".popup__add");
-const closePopupAdd=popupAdd.querySelector(".popup__close-add");
+const popupAdd=document.querySelector(".popup-add");
+const closePopupAdd=popupAdd.querySelector(".popup-add__close");
 
 const elements=document.querySelector(".elements");
 const popupImage=document.querySelector(".popup-image");
