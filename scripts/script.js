e3aeac760f56d2d2e3ba4823c84fcf7a632f116f
Christian Sales <christiansales07@outlook.com>
2023-11-08 22:20:42 -0300

    adicionando novas funcionalidades

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,12 @@ const profileDescription=profile.querySelector(".profile__description");
 const popupName=popup.querySelector(".popup__input_name");
 const popupDescription=popup.querySelector(".popup__input_description");
 const buttonSave=popup.querySelector(".popup__button");
+const buttonAdd=profile.querySelector(".profile__button-add");
+const popupAdd=document.querySelector(".popup-add");
+const closePopupAdd=popupAdd.querySelector(".popup__close-add");
 
 const elements=document.querySelector(".elements");
-const buttonLike=elements.querySelectorAll(".elements__like");
+const popupImage=document.querySelector(".popup-image");
 
 function editForm(){
  popup.classList.toggle("popup_opened");
@@ -31,11 +34,99 @@ function handleProfileFormSubmit(evt){
 
 popup.addEventListener('submit', handleProfileFormSubmit);
 
-function likeActive() {
-  this.setAttribute("src", "./images/likeactive.png");
-  return;
+function addCard(card){
+  const templateCard=document.querySelector("#template-card");
+
+  const cardElement=templateCard.content.cloneNode(true);
+
+  const elementImg= cardElement.querySelector(".elements__image");
+  elementImg.src=card.link;
+  elementImg.alt=card.name;
+
+  const elementName=cardElement.querySelector(".elements__title");
+   elementName.textContent=card.name;
+
+  elementImg.addEventListener("click", ()=>{
+    const imgZoom=popupImage.querySelector(".popup__img-zoom");
+    imgZoom.src=card.link;
+    const popupName=popupImage.querySelector(".popup__name");
+    popupName.textContent=card.name;
+    popupImage.classList.add("popup_opened");
+  });
+
+  const closePopupZoom=popupImage.querySelector(".popup__close-zoom");
+  closePopupZoom.addEventListener("click", ()=>{
+     popupImage.classList.remove("popup_opened");
+  });
+
+const elementTrash =cardElement.querySelector(".elements__trash");
+elementTrash.addEventListener("click", ()=>{
+  const removeElement = elementTrash.closest(".elements__card");
+  removeElement.remove();
+});
+
+const elementList=elements.querySelector(".elements__list");
+  elementList.prepend(cardElement);
+
+const likeButton = document.querySelector(".elements__button-like");
+likeButton.addEventListener("click", () => {
+  likeButton.classList.toggle("elements__button-like_click");
+});
 }
 
-buttonLike.forEach(function(element) {
-  element.addEventListener("click", likeActive);
-});
+
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+initialCards.forEach(addCard);
+
+const inputTitle= popupAdd.querySelector(".popup-add__input_title");
+const inputImage = popupAdd.querySelector(".popup-add__input_link");
+
+function addImage(){
+  popupAdd.classList.toggle("popup_opened");
+  inputTitle.value = "";
+  inputImage.value = "";
+}
+buttonAdd.addEventListener("click", addImage);
+closePopupAdd.addEventListener("click", addImage);
+
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+
+  const card = {
+    name: inputTitle.value,
+    link: inputImage.value,
+  };
+  addCard(card);
+  popupAdd.classList.remove("popup_opened");
+
+}
+
+popupAdd.addEventListener("submit", handleCardFormSubmit);
